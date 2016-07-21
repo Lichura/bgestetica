1129682e709c0b9dae79d9b2eef3eafb2910ecef
Lisandro Gurman <lichun88@gmail.com>
2016-07-21 18:28:56 +0200

    quiero subirlo a heroku

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module BgEstetica
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.initialize_on_precompile = false
   end
 end
