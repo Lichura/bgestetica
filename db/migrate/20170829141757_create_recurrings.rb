dd3cc0369d7da7e2902d91777f82bb12dcb67585
Lisandro Gurman <lichun88@gmail.com>
2017-11-04 16:08:22 +0100

    se corrigio un integer de 13 bytes@

--- db/migrate/20170829141757_create_recurrings.rb
+++ db/migrate/20170829141757_create_recurrings.rb
@@ -2,7 +2,7 @@ class CreateRecurrings < ActiveRecord::Migration[5.0]
   def change
     create_table :recurrings do |t|
       t.string :rec_type
-      t.integer :event_lenght,  :limit => 13 
+      t.string :event_lenght
       t.integer :event_pid
       t.datetime :start_date
       t.datetime :end_date
